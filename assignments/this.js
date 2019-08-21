d3e6b3295e0644217e141363441ea615966658cb
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-08-21 15:55:56 +0100

    principle 2 added example

--- assignments/this.js
+++ assignments/this.js
@@ -2,7 +2,7 @@
 // in your own words. explain the four principle for the "this" keyword below.
 //*
 //The principles of 'this' is that this will bind to specific objects in different SVGElementInstanceList. below are the principles of 'this' 
-// 1. Global Object Binding:
+// 1. Window/Global Object Binding:
 //When in the global scope, the value of “this” will be the window/console Object.
 // 2. Implicit Binding:
 // Implicit binding occurs when dot notation is used to invoke a function.
@@ -10,17 +10,28 @@
 // When you create an instance of a function, the context of this moves to the newly created instance.
 // 4. Explicit Binding 
 // Explicit binding occurs when .call(), .apply(), or .bind() are used on a function. It is called explicit because you are explicitly passing in a this context to call() or apply()
-*
-* write out a code example of each explanation above
-
+//write out a code example of each explanation above
 
 // Principle 1
 
 // code example for Window Binding
+function myReligion(religion) {
+    console.log(this);
+    return religion;
+  };
+  myReligion("Christian");
 
 // Principle 2
 
 // code example for Implicit Binding
+const myName = {
+    greeting: 'Hello',
+    sayHello: function(name) {
+      console.log(`${this.greeting} my name is ${name}`);
+      console.log(this);
+    }
+  };
+  myName.sayHello('Atinuke');
 
 // Principle 3
 
